4f7da229acb87bbf848586c03b2b903570eff33a
Bullerian <buller@bullerssystem.com>
2017-05-20 12:59:54 +0300

    isEven added to main

--- main.c
+++ main.c
@@ -2,7 +2,14 @@
 
 int main(void)
 {
-	printf("hello all!\n");
+  int testArr[]={0,2,5,13,44,57,63,90};
+  int i;
+
+  for(i=0; i < (sizeof(testArr)/sizeof(int));i++)
+  {
+    printf("%d is %s number\n", testArr[i], isEven(testArr[i])?"odd":"even");
+  }
+
 }
 
 
